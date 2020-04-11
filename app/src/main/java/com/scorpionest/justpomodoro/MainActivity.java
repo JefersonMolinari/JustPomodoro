0db075d7900467ada416202a8010227ef0617472
Jeferson Molinari <jeferson.bdc@hotmail.com>
2020-04-11 14:10:27 -0700

    JP006 - feat: Add settings menu - BE

--- app/src/main/java/com/scorpionest/justpomodoro/MainActivity.java
+++ app/src/main/java/com/scorpionest/justpomodoro/MainActivity.java
@@ -3,8 +3,10 @@ package com.scorpionest.justpomodoro;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.preference.PreferenceManager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.Menu;
@@ -13,30 +15,35 @@ import android.view.View;
 
 import com.scorpionest.justpomodoro.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener{
 
     ActivityMainBinding bd;
 
-    private static final int TIME_MIN_POMODORO = 25;
-    private static final int TIME_MIN_SHORT_BREAK = 5;
-    private static final int TIME_MIN_LONG_BREAK = 15;
     private static final int TIME_RATE_MILISEC = 1000;
     private static final int TIME_MIN_TO_SEC = 60;
-    private static final int SECTIONS_TO_LONG_BREAK = 40;
-
     private static final String DISPLAY_DISPLAY = "%02d:%02d";
     private static final String DISPLAY_START = "%02d:00";
-    private static final String CYCLE_POMODORO = "activity_pomodoro";
-    private static final String CYCLE_BREAK = "activity_break";
+    private static final String CYCLE_POMODORO = "cycle_pomodoro";
+    private static final String CYCLE_BREAK = "cycle_break";
 
-    private int timerMinutes;
-    private int timerSeconds;
+    private int timeMinPomodoro;
+    private int timeMinShortBreak;
+    private int timeMinLongBreak;
+    private int sectionsToLongBreak;
+
+    private boolean autoStart;
+    private boolean isRunning;
+
+    private String soundPomodoro;
+    private String soundShortBreak;
+    private String soundLongBreak;
+
     private int counterInSecs;
     private int counterPomodoro;
 
     private String currentCycle = "";
 
-    CountDownTimer timer;
+    private CountDownTimer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,15 +52,14 @@ public class MainActivity extends AppCompatActivity {
 
         bd = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        setupSharedPreferences();
+
         nextSection();
 
         bd.buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bd.buttonStart.setVisibility(View.INVISIBLE);
-                bd.buttonPause.setVisibility(View.VISIBLE);
-                timer = startCounter();
-                timer.start();
+                startPomodoro();
             }
         });
 
@@ -86,6 +92,71 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    private void setupSharedPreferences() {
+
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(this);
+
+        autoStart = sharedPreferences.getBoolean(getString(R.string.settings_auto_start_timer_key),
+                getResources().getBoolean(R.bool.settings_auto_start_timer_default));
+
+        loadTimePomodoroFromSharedPreferences(sharedPreferences);
+        loadTimeShortBreakFromSharedPreferences(sharedPreferences);
+        loadTimeLongBreakFromSharedPreferences(sharedPreferences);
+        loadTimeLongBreakFreqFromSharedPreferences(sharedPreferences);
+
+        loadSoundPomodoroFromSharedPreferences(sharedPreferences);
+        loadSoundShortBreakFromSharedPreferences(sharedPreferences);
+        loadSoundLongBreakFromSharedPreferences(sharedPreferences);
+
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    private void loadTimePomodoroFromSharedPreferences(SharedPreferences sharedPreferences) {
+        timeMinPomodoro = Integer.parseInt(sharedPreferences.getString(getString(R.string.settings_pomodoro_time_key),
+                getString(R.string.settings_pomodoro_time_default)));
+        if (!isRunning) {
+            bd.textViewCounter.setText(String.format(DISPLAY_START, timeMinPomodoro));
+        }
+    }
+
+    private void loadTimeShortBreakFromSharedPreferences(SharedPreferences sharedPreferences) {
+        timeMinShortBreak = Integer.parseInt(sharedPreferences.getString(getString(R.string.settings_short_break_time_key),
+                getString(R.string.settings_short_break_time_default)));
+    }
+
+    private void loadTimeLongBreakFromSharedPreferences(SharedPreferences sharedPreferences) {
+        timeMinLongBreak = Integer.parseInt(sharedPreferences.getString(getString(R.string.settings_long_break_time_key),
+                getString(R.string.settings_long_break_time_default)));
+    }
+
+    private void loadTimeLongBreakFreqFromSharedPreferences(SharedPreferences sharedPreferences) {
+        sectionsToLongBreak = Integer.parseInt(sharedPreferences.getString(getString(R.string.settings_long_break_freq_time_key),
+                getString(R.string.settings_long_break_freq_time_default)));
+    }
+
+    private void loadSoundPomodoroFromSharedPreferences(SharedPreferences sharedPreferences) {
+        soundPomodoro = sharedPreferences.getString(getString(R.string.settings_pomodoro_sound_key),
+                getString(R.string.settings_pomodoro_sound_default));
+    }
+
+    private void loadSoundShortBreakFromSharedPreferences(SharedPreferences sharedPreferences) {
+        soundShortBreak = sharedPreferences.getString(getString(R.string.settings_short_break_sound_key),
+                getString(R.string.settings_short_break_sound_default));
+    }
+
+    private void loadSoundLongBreakFromSharedPreferences(SharedPreferences sharedPreferences) {
+        soundLongBreak = sharedPreferences.getString(getString(R.string.settings_long_break_sound_key),
+                getString(R.string.settings_long_break_sound_default));
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -102,24 +173,37 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void startPomodoro() {
+        bd.buttonStart.setVisibility(View.INVISIBLE);
+        bd.buttonPause.setVisibility(View.VISIBLE);
+        timer = startCounter();
+        timer.start();
+    }
+
     private CountDownTimer startCounter() {
+        isRunning = true;
         return new CountDownTimer(counterInSecs*TIME_RATE_MILISEC, TIME_RATE_MILISEC) {
             @Override
             public void onTick(long millisUntilFinished) {
-                timerMinutes = counterInSecs / TIME_MIN_TO_SEC;
-                timerSeconds = counterInSecs % TIME_MIN_TO_SEC;
+                int timerMinutes = counterInSecs / TIME_MIN_TO_SEC;
+                int timerSeconds = counterInSecs % TIME_MIN_TO_SEC;
                 bd.textViewCounter.setText(String.format(DISPLAY_DISPLAY, timerMinutes, timerSeconds));
                 counterInSecs--;
+
             }
 
             @Override
             public void onFinish() {
                 nextSection();
+                if (autoStart) {
+                    startPomodoro();
+                }
             }
         };
     }
 
     private void nextSection() {
+        isRunning = false;
         bd.buttonStart.setVisibility(View.VISIBLE);
         bd.buttonPause.setVisibility(View.INVISIBLE);
 
@@ -128,18 +212,38 @@ public class MainActivity extends AppCompatActivity {
         if (currentCycle.equals(CYCLE_POMODORO)) {
             counterPomodoro++;
             currentCycle = CYCLE_BREAK;
-            if ((counterPomodoro % SECTIONS_TO_LONG_BREAK) != 0) {
-                counterInMins = TIME_MIN_SHORT_BREAK;
+            if ((counterPomodoro % sectionsToLongBreak) != 0) {
+                counterInMins = timeMinShortBreak;
+                bd.textViewCycle.setText("SHORT BREAK");
             } else {
-                counterInMins = TIME_MIN_LONG_BREAK;
+                counterInMins = timeMinLongBreak;
+                bd.textViewCycle.setText("LONG BREAK");
             }
         } else {
             currentCycle = CYCLE_POMODORO;
-            counterInMins = TIME_MIN_POMODORO;
+            counterInMins = timeMinPomodoro;
+            bd.textViewCycle.setText("POMODORO");
         }
 
         counterInSecs = counterInMins * TIME_MIN_TO_SEC;
         bd.textViewCounter.setText(String.format(DISPLAY_START, counterInMins));
         bd.textViewCounterPomos.setText(String.format("%d Pomodoro%s",counterPomodoro, counterPomodoro == 1 ? "" : "s"));
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(getString(R.string.settings_auto_start_timer_key))) {
+            autoStart = sharedPreferences.getBoolean(key, getResources().getBoolean(R.bool.settings_auto_start_timer_default));
+        } else if (key.equals(getString(R.string.settings_pomodoro_sound_key))) {
+            loadSoundPomodoroFromSharedPreferences(sharedPreferences);
+        } else if (key.equals(getString(R.string.settings_pomodoro_time_key))) {
+            loadTimePomodoroFromSharedPreferences(sharedPreferences);
+        } else if (key.equals(getString(R.string.settings_short_break_time_key))) {
+            loadTimeShortBreakFromSharedPreferences(sharedPreferences);
+        } else if (key.equals(getString(R.string.settings_long_break_time_key))) {
+            loadTimeLongBreakFromSharedPreferences(sharedPreferences);
+        } else if (key.equals(getString(R.string.settings_long_break_freq_time_key))) {
+            loadTimeLongBreakFreqFromSharedPreferences(sharedPreferences);
+        }
     }
 }
--- app/src/main/java/com/scorpionest/justpomodoro/SettingsFragment.java
+++ app/src/main/java/com/scorpionest/justpomodoro/SettingsFragment.java
@@ -1,13 +1,136 @@
 package com.scorpionest.justpomodoro;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.EditTextPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements OnSharedPreferenceChangeListener, OnPreferenceChangeListener {
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_visualizer);
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        int count = prefScreen.getPreferenceCount();
+
+        for (int i = 0; i < count; i++) {
+            Preference preference = prefScreen.getPreference(i);
+            if (!(preference instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(preference.getKey(),"");
+                setPreferenceSummary(preference, value);
+            }
+        }
+
+        Preference preference = findPreference(getString(R.string.settings_pomodoro_time_key));
+        preference.setOnPreferenceChangeListener(this);
+
+        preference = findPreference(getString(R.string.settings_short_break_time_key));
+        preference.setOnPreferenceChangeListener(this);
+
+        preference = findPreference(getString(R.string.settings_long_break_time_key));
+        preference.setOnPreferenceChangeListener(this);
+
+        preference = findPreference(getString(R.string.settings_long_break_freq_time_key));
+        preference.setOnPreferenceChangeListener(this);
+
+        final Toast toast = Toast.makeText(getContext(), "To Be Implemented", Toast.LENGTH_SHORT);
+
+        toBeImplementedToast(toast, getString(R.string.settings_config_do_not_disturb_mode_key));
+        toBeImplementedToast(toast, getString(R.string.settings_config_vibrate_key));
+        toBeImplementedToast(toast, getString(R.string.settings_config_rotate_key));
+        toBeImplementedToast(toast, getString(R.string.settings_config_prevent_screen_lock_key));
+        toBeImplementedToast(toast, getString(R.string.settings_sound_timer_start_key));
+        toBeImplementedToast(toast, getString(R.string.settings_sound_timer_end_key));
+        toBeImplementedToast(toast, getString(R.string.settings_pomodoro_sound_key));
+        toBeImplementedToast(toast, getString(R.string.settings_short_break_sound_key));
+        toBeImplementedToast(toast, getString(R.string.settings_long_break_sound_key));
+    }
+
+    private void toBeImplementedToast(final Toast toast, String key) {
+        Preference preference = findPreference(key);
+        preference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                toast.show();
+                return false;
+            }
+        });
+    }
+
+    private void setPreferenceSummary(Preference preference, String value) {
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(value);
+            if (prefIndex >= 0) {
+                listPreference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else if (preference instanceof EditTextPreference) {
+            preference.setSummary(value);
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if (null != preference) {
+            if (!(preference instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(preference.getKey(),"");
+                setPreferenceSummary(preference, value);
+            }
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Toast error = Toast.makeText(getContext(), "Please enter a number higher than 0", Toast.LENGTH_SHORT);
+
+        String pomodoroTimeKey = getString(R.string.settings_pomodoro_time_key);
+        String shortBreakTimeKey = getString(R.string.settings_short_break_time_key);
+        String longBreakTimeKey = getString(R.string.settings_long_break_time_key);
+        String longBreakFreqTimeKey = getString(R.string.settings_long_break_freq_time_key);
+        String preferenceKey = preference.getKey();
+        if (preferenceKey.equals(pomodoroTimeKey) ||
+                preferenceKey.equals(shortBreakTimeKey) ||
+                preferenceKey.equals(longBreakTimeKey) ||
+                preferenceKey.equals(longBreakFreqTimeKey)) {
+            String stringTime = (String) newValue;
+            try {
+                int time = Integer.parseInt(stringTime);
+                if (time <= 0) {
+                    error.show();
+                    return false;
+                }
+            } catch (NumberFormatException nfe) {
+                error.show();
+                return false;
+            }
+        }
+        return true;
     }
 }
